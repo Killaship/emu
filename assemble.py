b0808493bd171fdf3289899d093a975514499ea5
William <69988679+Killaship@users.noreply.github.com>
2023-03-20 22:29:56 -0400

    Create assemble.py

---
+++ assemble.py
@@ -0,0 +1,35 @@
+# avengers assemble :^)
+
+"""
+iterate over each line
+if it's blank or contains no tokens, skip it
+trim off everything after a comment mark
+if it has stuff, don't skip it
+turn the code into the opcode and operands
+repeat for the rest of the lines
+translate the opcodes and operands into something the cpu can run
+"""
+
+import os
+
+def load(filename):
+    pass
+
+
+
+def info(): # add more later
+    print("This is an assembler for Killaship's custom CPU arch!")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    info()
+    load(input("Filename?"))
+    # parsetokens()
+    # translate()
+    # genfile()
+  
